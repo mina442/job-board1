28da34e5c9f9bfaa29dc14a036e9940fe876ff0f
mina442 <gamil7256@gmail.com>
2020-10-21 13:59:53 -0700

    first commit

---
+++ job/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.utils.text import slugify
+
+# Create your models here.
+JOB_TYBE = (('Full Time','Full Time'),('Part Time','Part Time'),)
+def image_upload(instance, filename):
+    imagename, extension = filename.split(".")
+    return "jobs/%s.%s"%(instance.id,extension)
+
+class Job(models.Model):
+    title = models.CharField(max_length=100)
+    job_tybe = models.CharField(max_length=15 , choices=JOB_TYBE)
+    description = models.TextField(max_length=1000)
+    published_at = models.DateTimeField(auto_now=True)
+    vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    experience = models.IntegerField(default=1)
+    category = models.ForeignKey('Category', on_delete= models.CASCADE)
+    image = models.ImageField(upload_to=image_upload)
+    slug = models.SlugField(blank=True, null=True)
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Job,self).save(*args, **kwargs)
+
+
+    def __str__(self):
+         return self.title
+class Category(models.Model):
+     name = models.CharField(max_length=25)
+     def __str__(self):
+         return self.name
+
+
+
---
+++ job/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path ,include
+from . import views
+app_name ='job'
+urlpatterns = [
+    path('',views.job_list),
+    path('<str:slug>',views.job_detail, name='job_detail') ,
+]
